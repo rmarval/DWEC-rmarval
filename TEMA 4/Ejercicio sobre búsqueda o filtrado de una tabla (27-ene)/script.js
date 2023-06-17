2291b4a0478e29ccb568584e71e317abfc975f19
rmarval <106530062+rmarval@users.noreply.github.com>
2023-06-17 19:37:49 +0200

    script.js

--- TEMA 4/Ejercicio sobre búsqueda o filtrado de una tabla (27-ene)/script.js
+++ TEMA 4/Ejercicio sobre búsqueda o filtrado de una tabla (27-ene)/script.js
@@ -1,4 +1,4 @@
-//Si le ponemos a esa tabla el id gridDatos, ponemos empezar nuestro código añadiendo un campo input de esta forma: 
+
 
 const tabla = document.getElementById('gridDatos');
 const buscar = document.createElement('input');
@@ -6,7 +6,7 @@ buscar.setAttribute('placeholder', 'Introduce tu búsqueda');
 buscar.setAttribute('type', 'text');
 tabla.parentElement.prepend(buscar);
 
-//Posteriormente, añadiremos el evento input para filtrar las filas a mostrar así
+//añado el evento input para filtrar las filas a mostrar así
 
 buscar.addEventListener('input', evento => {
     const textoBusqueda = evento.target.value.toLowerCase();
